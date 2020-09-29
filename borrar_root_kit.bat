ba5e0844b394ff3b45bdba5d9842bf7ac664a67f
Alex AC <69085537+alexayca@users.noreply.github.com>
2020-09-28 23:26:07 -0500

    Create borrar_root_kit.bat

---
+++ borrar_root_kit.bat
@@ -0,0 +1,9 @@
+
+CLS 
+ATTRIB -R -A -S -H /S /D E:/*.* 
+PROMT Tlacticpac Toquichtin tiez $_ $d $t 
+IF EXIST E:/AUTORUN.INF DEL E:/AUTORUN.INF
+IF EXIST *.lnk DEL *.lnk
+cls 
+
+// Cambiar la letra de la unidad
